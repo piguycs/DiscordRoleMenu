f44fe7810103191a32afa3cac7fc415ab1a8af54
RocKing1001 <44892832+RocKing1001@users.noreply.github.com>
2020-11-13 23:46:52 +0100

    Perfected the bot script
    Made the bot script to be able to store data in a file

--- RoleMenu/src/bot.js
+++ RoleMenu/src/bot.js
@@ -3,6 +3,16 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const fs = require('fs')
+
+const storeData = (data, path) => {
+    try {
+        fs.writeFileSync(path, JSON.stringify(data, null, 2))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,16 +22,19 @@ client.on('message', msg => {
     console.log(msg.content);
 });
 
+
+// Write JSON stuff to file
+client.on('message', msg => {
+    storeData(msg.guild.roles, "temp.json");
+    console.log(msg.guild.roles)
+});
+
 client.on('message', msg => {
     if (msg.content === 'ping') {
         msg.reply('Pong!');
     }
 });
 
-client.on('message', msg => {
-    if (msg.content === 'role') {
-        console.log(msg.guild.roles);
-    }
-});
 
+// login
 client.login(TOKEN);
