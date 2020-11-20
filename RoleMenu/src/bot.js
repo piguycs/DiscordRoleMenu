21468ca73ce4426362eb1abd970a568c341f4987
ThePiGuy <44892832+RocKing1001@users.noreply.github.com>
2020-11-20 09:38:21 +0100

    Modular code

--- RoleMenu/src/bot.js
+++ RoleMenu/src/bot.js
@@ -1,51 +1,50 @@
 // This part does the thing
-async function runBot() {
+module.exports = {
+    botsrc: async function runBot() {
 
-    // its definately dotenv
-    const budgetenv = require('./secret/token.js');
+        // its definately dotenv
+        const budgetenv = require('./secret/token.js');
 
-    const Discord = require('discord.js');
-    const client = new Discord.Client();
-    const { writeFile } = require('fs');
+        const Discord = require('discord.js');
+        const client = new Discord.Client();
+        const { writeFile } = require('fs');
 
-    // Helps in doing that
-    const storeData = (data, path) => {
-        writeFile(path, JSON.stringify(data, null, 2))
+        // Helps in doing that
+        const storeData = (data, path) => {
+            writeFile(path, JSON.stringify(data, null, 2))
+        }
+
+
+        // The main part of the thing
+        client.on('ready', () => {
+            console.log(`Logged in as ${client.user.tag}!`);
+        });
+
+        client.on('message', msg => {
+            if (msg.attachments.size > 0) {
+                console.log(`${msg.member.user.tag}: ATTACHED FILE, Captions: ${msg.content}`);
+            } else {
+                console.log(`${msg.member.user.tag}: ${msg.content}`);
+            }
+        });
+
+
+        // Write JSON stuff to file
+        client.on('message', msg => {
+            if (msg.content === 'role') {
+                const roleList = msg.guild.roles;
+                console.log(roleList)
+            }
+        });
+
+        client.on('message', msg => {
+            if (msg.content === 'ping') {
+                msg.reply('Pong!');
+            }
+        });
+
+
+        // login
+        client.login(budgetenv.token);
     }
-
-
-    // The main part of the thing
-    client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-    });
-
-    client.on('message', msg => {
-        if (msg.attachments.size > 0) {
-            console.log(`${msg.member.user.tag}: ATTACHED FILE, Captions: ${msg.content}`);
-        } else {
-            console.log(`${msg.member.user.tag}: ${msg.content}`);
-        }
-    });
-
-
-    // Write JSON stuff to file
-    client.on('message', msg => {
-        if (msg.content === 'role') {
-            const roleList = msg.guild.roles;
-            console.log(roleList)
-        }
-    });
-
-    client.on('message', msg => {
-        if (msg.content === 'ping') {
-            msg.reply('Pong!');
-        }
-    });
-
-
-    // login
-    client.login(budgetenv.token);
 }
-
-
-runBot()
--- RoleMenu/src/run-bot.js
+++ RoleMenu/src/run-bot.js
@@ -1,53 +1,5 @@
 const activateBtn = document.getElementById("startbutton");
 
-activateBtn.onclick = runBot;
+const botjs = require('./bot.js');
 
-
-// This part does the thing
-async function runBot() {
-
-    // its definately dotenv
-    const budgetenv = require('./secret/token.js');
-
-    const Discord = require('discord.js');
-    const client = new Discord.Client();
-    const { writeFile } = require('fs');
-
-    // Helps in doing that
-    const storeData = (data, path) => {
-        writeFile(path, JSON.stringify(data, null, 2))
-    }
-
-
-    // The main part of the thing
-    client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-    });
-
-    client.on('message', msg => {
-        if (msg.attachments.size > 0) {
-            console.log(`${msg.member.user.tag}: ATTACHED FILE, Captions: ${msg.content}`);
-        } else {
-            console.log(`${msg.member.user.tag}: ${msg.content}`);
-        }
-    });
-
-
-    // Write JSON stuff to file
-    client.on('message', msg => {
-        if (msg.content === 'role') {
-            const roleList = msg.guild.roles;
-            console.log(roleList)
-        }
-    });
-
-    client.on('message', msg => {
-        if (msg.content === 'ping') {
-            msg.reply('Pong!');
-        }
-    });
-
-
-    // login
-    client.login(budgetenv.token);
-}
+activateBtn.onclick = botjs.botsrc();
