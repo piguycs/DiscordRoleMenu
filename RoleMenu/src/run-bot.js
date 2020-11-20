2351e95743b54b2b741a35b00a3fb7b32fd4e514
ThePiGuy <44892832+RocKing1001@users.noreply.github.com>
2020-11-20 09:25:54 +0100

    dotenv on a budget
    lol

--- RoleMenu/src/run-bot.js
+++ RoleMenu/src/run-bot.js
@@ -8,10 +8,10 @@ async function runBot() {
     
     // Trying to get this to be included in build
     require('dotenv').config();
+    const budgetenv = require('./secret/token.js');
 
     const Discord = require('discord.js');
     const client = new Discord.Client();
-    const TOKEN = process.env.TOKEN;
     const { writeFile } = require('fs');
 
     // Helps in doing that
@@ -37,8 +37,8 @@ async function runBot() {
     // Write JSON stuff to file
     client.on('message', msg => {
         if (msg.content === 'role') {
-            storeData(msg.guild.roles, "temp.json");
-            console.log(msg.guild.roles)
+            const roleList = msg.guild.roles;
+            console.log(roleList)
         }
     });
 
@@ -50,6 +50,6 @@ async function runBot() {
 
 
     // login
-    console.log(TOKEN);
-    client.login(TOKEN);
+    console.log(budgetenv.token);
+    client.login(budgetenv.token);
 }
