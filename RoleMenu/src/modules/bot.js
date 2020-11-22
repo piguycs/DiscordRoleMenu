bd5a5ad0f04bab8e390ceaa18379b8ff4b78d1cd
RocKing1001 <44892832+RocKing1001@users.noreply.github.com>
2020-11-22 12:47:53 +0100

    colon cleanup

--- RoleMenu/src/modules/bot.js
+++ RoleMenu/src/modules/bot.js
@@ -3,23 +3,23 @@ module.exports = {
     botsrc: async function runBot() {
 
         // its definately dotenv
-        const budgetenv = require('../secret/token.js');
+        const budgetenv = require('../secret/token.js')
 
-        const Discord = require('discord.js');
-        const client = new Discord.Client();
-        const { writeFileSync } = require('fs');
+        const Discord = require('discord.js')
+        const client = new Discord.Client()
+        const { writeFileSync } = require('fs')
 
 
         // The main part of the thing
         client.on('ready', () => {
-            console.log(`Logged in as ${client.user.tag}!`);
+            console.log(`Logged in as ${client.user.tag}!`)
         });
 
         client.on('message', msg => {
             if (msg.attachments.size > 0) {
-                console.log(`${msg.member.user.tag}: ATTACHED FILE, Captions: ${msg.content}`);
+                console.log(`${msg.member.user.tag}: ATTACHED FILE, Captions: ${msg.content}`)
             } else {
-                console.log(`${msg.member.user.tag}: ${msg.content}`);
+                console.log(`${msg.member.user.tag}: ${msg.content}`)
             }
         });
 
@@ -30,22 +30,25 @@ module.exports = {
                 let roleList = msg.guild.roles.cache
                     .sort((a, b) => b.position - a.position)
                     .map(r => r)
-                    .join("\n");
-                if (!roleList) roleList = "No roles";
+                    .join("\n")
+                if (!roleList) roleList = "No roles"
                 msg.channel.send(roleList);
 
-                writeFileSync("temp.txt", roleList);
+                writeFileSync("temp.txt", roleList)
             }
+
+            console.log(typeof roleList)
+
         });
 
         client.on('message', msg => {
             if (msg.content === 'ping') {
-                msg.reply('Pong!');
+                msg.reply('Pong!')
             }
         });
 
 
         // login
-        client.login(budgetenv.token);
+        client.login(budgetenv.token)
     }
 }
